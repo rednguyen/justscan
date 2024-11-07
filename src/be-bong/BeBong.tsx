ebe16afab3ba11f7ef769505d2047a36790ec4cb
Red Nguyen <rednguyen1997@gmail.com>
2024-11-07 15:45:24 -0600

    updated bebong

--- src/be-bong/BeBong.tsx
+++ src/be-bong/BeBong.tsx
@@ -4,21 +4,23 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   text-align: center;
   background: linear-gradient(to bottom, #FF1414 0%, #1cabf5 34.48%, #58068c 100%);
-  height: 110vh;
+  padding: 50px;
+  width: 100%;
 `;
 
 
 const Header = styled.div`
-  padding: 10vh;
+  padding: 10vw;
   background: linear-gradient(to bottom, #006ded 0%, #f5d2b8 40%, #00e2ed 100%);
-  font-size: 32px;
+  font-size: 7vw;
   color: #58068c;
   font-family:'Courier New', Courier, monospace;
-  font-weight: 1000;
+  font-weight: 800;
+  border-radius: 50%;
 `;
 
 const Img = styled.img`
-
+  width: 80%
 `;
 
 export default function BeBong () {
