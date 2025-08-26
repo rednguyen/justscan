6e9cded1afff03ad78ca6fec517dc3fbe22def3b
Red Nguyen <rednguyen1997@gmail.com>
2025-08-26 01:04:21 -0500

    added restaurant menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import MenuLalunaSpa from './menu-lalunaspa/MenuLalunaSpa';
 import PageNotFound from './page-not-found/PageNotFound';
 import BeBong from './be-bong/BeBong';
 import MenuLalunaTour from './menu-lalunatour/MenuLalunaTour';
+import LalunaRestaurant from './menu-lalunarestaurant/LalunaRestaurant';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import "./App.css"
 
@@ -14,6 +15,7 @@ export default function App() {
               <Route path="/main-menu/lalunaspa/" element={<MenuLalunaSpa/>}/>
               <Route path="/main-menu/bebong/" element={<BeBong/>}/>
               <Route path="/main-menu/lalunatour/" element={<MenuLalunaTour/>}/>
+              <Route path="/main-menu/lalunarestaurant/" element={<LalunaRestaurant/>}/>
               <Route
                     path="*" element={<PageNotFound />}/>
           </Routes>
---
+++ src/menu-lalunarestaurant/LalunaRestaurant.tsx
@@ -0,0 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import styled from 'styled-components';
+
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export default function LalunaRestaurant () {
+    
+    return (
+        <>  
+          <Img src='/lalunarestaurant/alacarte1.jpg'/>
+          <Img src='/lalunarestaurant/alacarte2.jpg'/>
+          <Img src='/lalunarestaurant/alacarte3.jpg'/>
+          <Img src='/lalunarestaurant/alacarte4.jpg'/>
+          <Img src='/lalunarestaurant/alacarte5.jpg'/>
+          <Img src='/lalunarestaurant/alacarte6.jpg'/>
+          <Img src='/lalunarestaurant/alacarte7.jpg'/>
+          <Img src='/lalunarestaurant/alacarte8.jpg'/>
+          <Img src='/lalunarestaurant/alacarte9.jpg'/>
+          <Img src='/lalunarestaurant/alacarte10.jpg'/>
+          <Img src='/lalunarestaurant/alacarte11.jpg'/>
+          <Img src='/lalunarestaurant/alacarte12.jpg'/>
+        </>
+    )
+}
